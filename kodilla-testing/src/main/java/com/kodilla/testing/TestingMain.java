601dc81d57c97e85489094eeb3f4413114875e84
Jarosław Kołacz <jaroslaw.kolacz@op.pl>
2024-02-08 15:59:03 +0100

    porawa zadania 6.1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,2 +1,19 @@
-package com.kodilla.testing;public class TestingMain {
+package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+
+
+public class TestingMain {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        Calculator calculator1 = new Calculator();
+
+        int result = calculator.addAToB(8, 7);
+        System.out.println(result);
+        System.out.println("Test Succes");
+        int result1 = calculator1.subtractAFromB(15, 7);
+
+        System.out.println(result1);
+        System.out.println("Test Succes");
+    }
 }
