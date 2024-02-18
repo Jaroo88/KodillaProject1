8666f47a643f249774a153be764092480d045f17
Jarosław Kołacz <jaroslaw.kolacz@op.pl>
2024-02-18 13:41:09 +0100

    CollectionTestSuitTest

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,2 +1,20 @@
-package com.kodilla.testing.collection;public class OddNumbersExterminator {
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddNumbersExterminator {
+
+    public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> library = new ArrayList<>();
+
+        for(int number : numbers) {
+            if(number % 2 == 0)
+            {
+                library.add(number);
+            }
+        }
+        return library;
+    }
 }
+    
