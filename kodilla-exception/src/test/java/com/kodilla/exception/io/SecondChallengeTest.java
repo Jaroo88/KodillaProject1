85c2dc387e143b86be78c4629bba124ec3fd7c2e
Jarosław Kołacz <jaroslaw.kolacz@op.pl>
2024-03-25 11:03:05 +0100

    CollectionTestSuitTest

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/SecondChallengeTest.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SecondChallengeTest {
+    @Test
+    void probablyIWillThrowExceptionTest() throws Exception {
+        // Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        // When
+        String result = secondChallenge.probablyIWillThrowException(1.0, 1.0);
+
+        // Then
+        assertEquals("Done!", result);
+    }
+}
+
