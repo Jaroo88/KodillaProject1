782209fa08d5e38e1a34a80f45d6cd239f7e0eb5
Jarosław Kołacz <jaroslaw.kolacz@op.pl>
2024-05-05 14:05:12 +0200

    CollectionTestSuitTest

---
+++ kodilla-spring/src/main/java/Shapes/Circle.java
@@ -0,0 +1,12 @@
+package Shapes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Circle implements Shape{
+    @Override
+    public String getShapeName() {
+        return "This is a circle.";
+    }
+
+}
---
+++ kodilla-spring/src/main/java/Shapes/ShapesFactory.java
@@ -0,0 +1,12 @@
+package Shapes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShapesFactory {
+    @Bean
+    public Square createSquare() {
+        return new Square();
+    }
+}
